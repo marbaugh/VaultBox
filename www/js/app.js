d5de897dacbbd030c635b12306b43bacf6109be5
Matthew Arbaugh <marbaugh@gmail.com>
2015-02-06 10:26:58 -0500

    Rebasing the project using SideMenu
    Rebasing the project to use sidemenu
    $ionic start myApp sidemenu

--- www/js/app.js
+++ www/js/app.js
@@ -1,52 +1,71 @@
-  // Ionic Starter App
+// Ionic Starter App
 
-  // angular.module is a global place for creating, registering and retrieving Angular modules
-  // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-  // the 2nd parameter is an array of 'requires'
-  angular.module('starter', ['ionic'])
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
 
-  .run(function($ionicPlatform) {
-       $ionicPlatform.ready(function() {
-          // Hide the accessory bar by default 
-          // (remove this to show the accessory bar above the keyboard for form inputs)
-          if(window.cordova && window.cordova.plugins.Keyboard) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-          }
-          if(window.StatusBar) {
-            StatusBar.styleDefault();
-          }
-        });
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('app', {
+    url: "/app",
+    abstract: true,
+    templateUrl: "templates/menu.html",
+    controller: 'AppCtrl'
   })
 
-  .config(function($stateProvider, $urlRouterProvider) {
-      $urlRouterProvider.otherwise('/')
-          $stateProvider
-          .state('home', {
-                 // url fragment with optional parameters
-                 url: '/',
-                 templateUrl: 'home.html'
-          })
-          .state('newEvent', {
-                 // url fragment with optional parameters
-                 url: '/newEvent',
-                 // Returns the path to the html template
-                 // that should be used by uiView.
-                 templateUrl: 'newEvent.html'
-          })
-          .state('viewEvents', {
-                 // url fragment with optional parameters
-                 url: '/viewEvents',
-                 // Returns the path to the html template
-                 // that should be used by uiView.
-                 templateUrl: 'viewEvents.html'
-          })
-          .state('event', {
-                 // url fragment with optional parameters
-                 url: '/event',
-                 // Returns the path to the html template
-                 // that should be used by uiView.
-                 templateUrl: 'event.html'
-          })
+  .state('app.search', {
+    url: "/search",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/search.html"
+      }
+    }
+  })
+
+  .state('app.browse', {
+    url: "/browse",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/browse.html"
+      }
+    }
+  })
+    .state('app.playlists', {
+      url: "/playlists",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/playlists.html",
+          controller: 'PlaylistsCtrl'
+        }
+      }
+    })
+
+  .state('app.single', {
+    url: "/playlists/:playlistId",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/playlist.html",
+        controller: 'PlaylistCtrl'
+      }
+    }
   });
-
-
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/playlists');
+});
